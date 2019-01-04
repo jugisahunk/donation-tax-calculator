78d39547e3c89efc860da1cd4b6688b44151242d
Jason Knight <jugisahunk@gmail.com>
2019-01-04 00:07:40 -0600

    added state tax calculations

--- jasmine/spec/TaxCalculatorSpec.js
+++ jasmine/spec/TaxCalculatorSpec.js
@@ -31,14 +31,132 @@ describe("Donation Calculator", function(){
                         "constant" : "6"
                     }
                 ]
+            },
+            "state" : {
+                "single" : [
+                    {
+                        "min_taxable_income" : "0",
+                        "max_taxable_income" : "10",
+                        "percentage" : ".01",
+                        "constant" : "0"
+                    },
+                    {
+                        "min_taxable_income" : "11",
+                        "max_taxable_income" : "50",
+                        "percentage" : ".05",
+                        "constant" : "2"
+                    }
+                ],
+                "married" : [
+                    {
+                        "min_taxable_income" : "0",
+                        "max_taxable_income" : "10",
+                        "percentage" : ".02",
+                        "constant" : "0"
+                    },
+                    {
+                        "min_taxable_income" : "11",
+                        "max_taxable_income" : "50",
+                        "percentage" : ".09",
+                        "constant" : "4.5"
+                    }
+                ]
             }
         }
 
         calculator = new Calculator(tax_brackets);
     });
 
-    describe("when desired credit, fed and state tax brackets and donation amount are known", function(){
-        it("should calculate net cost of donation", function(){
+    describe("given a taxable income and filing as 'single'", function(){
+        var filing_status, taxable_income;
+
+        beforeEach(function(){
+            filing_status = 1;
+            taxable_income = 5;
+        });
+
+        it("should calculate a federal tax amount (bracket % * taxable income) + bracket flat amount", function(){
+            //arrange
+            var expected_fed_tax = .4;
+
+            //act
+            actual_fed_tax = calculator.calculate_federal_tax(filing_status, taxable_income);
+
+            //assert
+            expect(actual_fed_tax).toEqual(expected_fed_tax);
+        });
+
+        it("should calculate a state tax amount (bracket % * taxable income) + bracket flat amount", function(){
+            //arrange
+            var expected_state_tax = .05;
+
+            //act
+            actual_state_tax = calculator.calculate_state_tax(filing_status, taxable_income);
+
+            //assert
+            expect(actual_state_tax).toEqual(expected_state_tax);
+        });
+    });
+
+    describe("given a taxable income and filing as 'married'", function(){
+        var filing_status, taxable_income;
+
+        beforeEach(function(){
+            filing_status = 2;
+                taxable_income = 35;
+        });
+
+        it("should calculate a federal tax amount (bracket % * taxable income) + bracket flat amount", function(){
+            //arrange
+            var expected_fed_tax = 10.2;
+
+            //act
+            actual_fed_tax = calculator.calculate_federal_tax(filing_status, taxable_income);
+
+            //assert
+            expect(actual_fed_tax).toEqual(expected_fed_tax);
+        });
+
+        it("should calculate a state tax amount (bracket % * taxable income) + bracket flat amount", function(){
+            //arrange
+            var expected_state_tax = 7.65;
+
+            //act
+            actual_state_tax = calculator.calculate_state_tax(filing_status, taxable_income);
+
+            //assert
+            expect(actual_state_tax).toEqual(expected_state_tax);
+        });
+    });
+
+    describe("given donation commitment of 1 year", function(){
+        it("should calculate a suggested donation amount twice the credit amount desired.", function(){
+            //arrange
+            var desired_credit = 1000, yearly_committment = 1;
+            var expected_donation_amount = 2000;
+
+            //act
+            var actual_donation_amount = calculator.calculate_suggested_donation_amount(desired_credit, yearly_committment);
+            //assert
+            expect(actual_donation_amount).toEqual(expected_donation_amount);
+        });
+    });
+
+    describe("given donation commitment of 2 years", function(){
+        it("should calculate a suggested donation amount 4/3rds the credit amount desired.", function(){
+            //arrange
+            var desired_credit = 1000, yearly_committment = 2;
+            var expected_donation_amount = 1334;
+
+            //act
+            var actual_donation_amount = calculator.calculate_suggested_donation_amount(desired_credit, yearly_committment);
+            //assert
+            expect(actual_donation_amount).toEqual(expected_donation_amount);
+        });
+    });
+
+    describe("given desired credit, fed and state tax amounts, and suggested donation amount", function(){
+        it("should calculate net cost of donation as desired credit + fed tax + state tax - suggested donation", function(){
             //arrange
             //credit + fed tax + state tax - donation
             var desired_credit = 5000,
@@ -52,62 +170,6 @@ describe("Donation Calculator", function(){
 
             //assert
             expect(actual_cost).toEqual(expected_cost);
-        });
-    });
-
-    describe("when committment is for 1 year", function(){
-        it("should calculate a suggested donation amount twice the credit amount desired.", function(){
-            //arrange
-            var desired_credit = 1000, yearly_committment = 1;
-            var expected_donation_amount = 2000;
-
-            //act
-            var actual_donation_amount = calculator.calculate_suggested_donation_amount(desired_credit, yearly_committment);
-            //assert
-            expect(actual_donation_amount).toEqual(expected_donation_amount);
-        });
-    });
-
-    describe("when committment is for 2 years", function(){
-        it("should calculate a suggested donation amount 4/3rds the credit amount desired.", function(){
-            //arrange
-            var desired_credit = 1000, yearly_committment = 2;
-            var expected_donation_amount = 1334;
-
-            //act
-            var actual_donation_amount = calculator.calculate_suggested_donation_amount(desired_credit, yearly_committment);
-            //assert
-            expect(actual_donation_amount).toEqual(expected_donation_amount);
-        });
-    });
-
-    describe("given a taxable income and filing as 'single'", function(){
-        it("should return a federal tax amount", function(){
-            //arrange
-            var filing_status = 1,
-                taxable_income = 5,
-                expected_fed_tax = .4;
-
-            //assert
-            actual_fed_tax = calculator.calculate_federal_tax(filing_status, taxable_income);
-
-            //act
-            expect(actual_fed_tax).toEqual(expected_fed_tax);
-        });
-    });
-
-    describe("given a taxable income and filing as 'married'", function(){
-        it("should return a federal tax amount", function(){
-            //arrange
-            var filing_status = 2,
-                taxable_income = 35,
-                expected_fed_tax = 10.2;
-
-            //assert
-            actual_fed_tax = calculator.calculate_federal_tax(filing_status, taxable_income);
-
-            //act
-            expect(actual_fed_tax).toEqual(expected_fed_tax);
         });
     });
     /*
--- jasmine/src/TaxCalculator.js
+++ jasmine/src/TaxCalculator.js
@@ -13,14 +13,26 @@ class Calculator{
         }
     }
 
+    calculate_state_tax(filing_status, taxable_income){
+        if(filing_status == 1){
+            var single_brackets = this.tax_brackets.state.single;
+            return this._calculate_tax(taxable_income, single_brackets);
+        }
+        else if(filing_status == 2){
+            var married_brackets = this.tax_brackets.state.married;
+            return this._calculate_tax(taxable_income, married_brackets);
+        }
+    }
+
     _calculate_tax(taxable_income, tax_brackets){
         for(var i=0; i < tax_brackets.length; i++){
-            var min_taxable_income = tax_brackets[i].min_taxable_income;
-            var max_taxable_income = tax_brackets[i].max_taxable_income;
+            var bracket_data = tax_brackets[i];
+            var min_taxable_income = bracket_data.min_taxable_income;
+            var max_taxable_income = bracket_data.max_taxable_income;
 
             if(taxable_income >= min_taxable_income && taxable_income <= max_taxable_income){
-                var percentage = parseFloat(tax_brackets[i].percentage);
-                var constant   = parseFloat(tax_brackets[i].constant);
+                var percentage = parseFloat(bracket_data.percentage);
+                var constant   = parseFloat(bracket_data.constant);
 
                 return taxable_income * percentage + constant;
             }
@@ -34,4 +46,5 @@ class Calculator{
     calculate_suggested_donation_amount(desired_credit, yearly_committment){
         return yearly_committment == 1 ? Math.ceil(desired_credit / .5) : Math.ceil(desired_credit / .75);
     }
+
 }
